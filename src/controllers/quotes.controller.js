80a2807d1557c05d58d57d4e0172969e79beb7a6
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2020-01-21 12:20:42 -0300

    change quotes controller to use database

--- src/controllers/quotes.controller.js
+++ src/controllers/quotes.controller.js
@@ -1,25 +1,32 @@
-const axios = require('axios');
-
-const QUOTE_API_URL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
-let quotes = [];
-
-(async function loadQuotes() {
-  try {
-    const {data} = await axios.get(QUOTE_API_URL);
-    quotes = data.quotes;
-  
-  } catch(err) {
-    console.error(err);
-  }
-})();
+const models = require('../../database/models');
 
 const index = async (req, res) => {
-  res.json(quotes);
+  try {
+    const quotes = await models.Quote.findAll({
+      attributes: ['quote', 'author']
+    });
+
+    return res.json(quotes);
+
+  } catch(err) {
+    console.error(err);
+    return res.status(500).json({ error: err.message });
+  }
 };
 
-const random = (req, res) => {
-  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-  res.json(randomQuote);
+const random = async (req, res) => {
+  try {
+    const quote = await models.Quote.findOne({
+      attributes: ['quote', 'author'],
+      order: models.sequelize.random()
+    });
+
+    return res.json(quote);
+
+  } catch(err) {
+    console.error(err);
+    return res.status(500).json({ error: err.message });
+  }
 };
 
 module.exports = {
